c2526543cb69fdaefae2bf4489fcd645c3cf6d62
Tofan <tofanalin1@gmail.com>
2024-05-04 17:16:02 +0300

    Created first api call

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\PostSeeder
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property Carbon|null $created_at
+ */
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+}
