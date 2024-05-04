31fbf04a4e77618b55d373d5c30c704a0cad9ce9
Tofan <tofanalin1@gmail.com>
2024-05-04 16:19:20 +0300

    Project initialization

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import {createApp} from "vue";
+import PostsIndex from './components/Posts/Index.vue';
+
+createApp({})
+    .component('PostsIndex', PostsIndex)
+    .mount('#app')
